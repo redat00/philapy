f85a40b2feddf02cedbfbf43e4ed2aef4a10171e
redat00 <contact@renaudduret.fr>
2022-09-18 17:59:51 +0200

    Build: Added CI/CD, Unittest, makeover of README.md

--- philapy/unittests/test_messager.py
+++ philapy/unittests/test_messager.py
@@ -9,6 +9,4 @@ from philapy.messager import Messager
 def test_messager_instanciation() -> None:
     """Test that messager instantiate correcly."""
     with raises(InvalidToken):
-        messager = Messager(
-            telegram_chat_id="test_chat_id", telegram_token="test_token"
-        )
+        Messager(telegram_chat_id="test_chat_id", telegram_token="test_token")
--- philapy/unittests/test_redis_handler.py
+++ philapy/unittests/test_redis_handler.py
@@ -1,6 +1,7 @@
+# First Party
 from philapy.redis_handler import RedisHandler
 
 
 def test_redis() -> None:
     """Test Redis."""
-    redis = RedisHandler(redis_url="127.0.0.1", redis_port=6379)
+    RedisHandler(redis_url="127.0.0.1", redis_port=6379)
